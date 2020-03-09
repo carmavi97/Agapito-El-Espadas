a09d0282100d92c5f579637ed29f0edbf565aeb7
carlos <carmavi97@gmail.com>
2020-03-09 13:59:11 +0100

    Main Menu

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    bool crouch = false;
+    float horizontalMove = 0f;
+    bool jump = false;
+    public float runSpeed = 16f;
+    void Start()
+    {
+
+        
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal")*runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if(Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+        
+    }
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove*Time.fixedDeltaTime,crouch,jump);
+        jump = false;
+        
+    }   
+
+}
---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audio;
+
+    public void SetVolume(float volume)
+    {
+        audio.SetFloat("Volume", volume);
+    }
+}
